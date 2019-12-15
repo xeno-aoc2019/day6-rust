168120f000e071c4c23be2131a2e14235d5ef8fb
xeno <xeno.freenode@gmail.com>
2019-12-15 14:50:33 +0100

    solved task 2

--- src/main.rs
+++ src/main.rs
@@ -24,35 +24,76 @@ impl PartialEq for SpaceObject {
     }
 }
 
-// impl Eq for SpaceObject {}
 
 fn main() {
+
+    // impl Eq for SpaceObject {}
+    let COM: SpaceObject = SpaceObject { name: "COM".into(), orbits: "COM".into() };
+
     println!("Hello, world!");
     let space_objects = read_deps();
-    let COM = SpaceObject { name: "COM".into(), orbits: "COM".into() };
+//    let COM = SpaceObject { name: "COM".into(), orbits: "COM".into() };
     let by_name = map_by_name(&space_objects);
     let by_satellite = map_by_satellite(&space_objects, &by_name);
 
     let o1 = SpaceObject { name: "foo".into(), orbits: "bar".into() };
     let o2 = SpaceObject { name: "foo".into(), orbits: "bar".into() };
 
-    println!("{} == {} => {}", o1, o2, o1 == o2);
+//    println!("{} == {} => {}", o1, o2, o1 == o2);
 
-    for key in by_satellite.keys() {
-        println!("{} ( {}", key, by_satellite.get(key).unwrap());
-    }
+//    for key in by_satellite.keys() {
+//        println!("{} ( {}", key, by_satellite.get(key).unwrap());
+//    }
     let by_center = map_by_center(&space_objects);
 
-
     let solution_1 = sum_orbits(&COM, 0, &by_center);
-    println!("Task 1: {}", solution_1)
+    println!("Task 1: {}", solution_1);
+
+    let solution_2 = distance_to_santa(&space_objects, &by_name, &by_satellite);
+    println!("Task 2: {}", solution_2);
+}
+
+fn distance_to_COM(space_object: &SpaceObject, by_satellite: &HashMap<SpaceObject, SpaceObject>) -> i32 {
+    if space_object.name == "COM" {
+        return 0;
+    }
+    1 + distance_to_COM(by_satellite.get(space_object).unwrap(), &by_satellite)
+}
+
+fn distance_to_santa(space_objects: &Vec<SpaceObject>, by_name: &HashMap<String, SpaceObject>, by_satellite: &HashMap<SpaceObject, SpaceObject>) -> i32 {
+    let you = by_name.get("YOU").unwrap();
+    let santa = by_name.get("SAN").unwrap();
+    let your_place = by_satellite.get(you).unwrap();
+    let santas_place = by_satellite.get(santa).unwrap();
+    let mut santa_to_COM = distance_to_COM(&santa, &by_satellite);
+    let mut you_to_COM = distance_to_COM(&you, &by_satellite);
+    let mut from_you = your_place;
+    let mut from_santa = santas_place;
+    let mut distance = 0;
+    while santa_to_COM > you_to_COM {
+        from_santa = by_satellite.get(from_santa).unwrap();
+        santa_to_COM = santa_to_COM - 1;
+        distance = distance + 1
+   }
+    while santa_to_COM < you_to_COM {
+        from_you = by_satellite.get(from_you).unwrap();
+        you_to_COM = you_to_COM - 1;
+        distance = distance + 1;
+    }
+    while from_you != from_santa {
+        from_santa = by_satellite.get(from_santa).unwrap();
+        from_you = by_satellite.get(from_you).unwrap();
+        distance = distance + 2;
+    }
+    println!("DISTANCES: santa->com: {} you->com: {} so-far: {}", santa_to_COM, you_to_COM, distance);
+    distance
 }
 
 
 fn sum_orbits(current: &SpaceObject, current_orbits: i32, by_center: &HashMap<&SpaceObject, Vec<&SpaceObject>>) -> i32 {
     let mut sum = current_orbits;
     for satellite in by_center.get(current).unwrap() {
-        sum += sum_orbits(&satellite, current_orbits+1, &by_center);
+        sum += sum_orbits(&satellite, current_orbits + 1, &by_center);
     }
     sum
 }
@@ -70,7 +111,7 @@ fn map_by_center(objects: &Vec<SpaceObject>) -> HashMap<&SpaceObject, Vec<&Space
     for center in objects {
         let mut satellites = vec!();
         for other in objects {
-            if other.orbits == center.name && other.name != center.name {
+            if (other.orbits == center.name) & (other.name != center.name) {
                 satellites.push(other);
             }
         }
@@ -85,7 +126,7 @@ fn map_by_satellite(objects: &Vec<SpaceObject>, by_name: &HashMap<String, SpaceO
         let orbits = by_name.get(&o.orbits);
         match orbits {
             Some(center) => {
-                println!("found {}", center);
+//                println!("found {}", center);
                 map.insert(o.clone(), center.clone());
             }
             None => {
